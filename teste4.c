23515309218691fdaebd2e074abf521c272e48b1
Wesley <wesley.marchi@itaipuparquetec.org.br>
2025-05-12 23:04:32 -0300

    refactor

--- teste4.c
+++ teste4.c
@@ -1,22 +1,38 @@
 #include <stdio.h>
+#include <string.h>
+
+#define QTD_ALUNOS 3
 
 typedef struct {
-    char nome[30];
+    char nome[50];
     int idade;
 } Aluno;
 
 int main() {
     FILE *fp = fopen("alunos.txt", "w");
+    if (fp == NULL) {
+        printf("Erro ao abrir o arquivo");
+        return 1;
+    }
+
     Aluno a;
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < QTD_ALUNOS; i++) {
+        printf("Aluno %d\n", i + 1);
+
         printf("Nome: ");
-        scanf(" %[^\n]", a.nome);
+        fgets(a.nome, sizeof(a.nome), stdin);
+        a.nome[strcspn(a.nome, "\n")] = '\0'; // Remove o '\n' do final
+
         printf("Idade: ");
         scanf("%d", &a.idade);
+        getchar(); // Limpa o '\n' deixado pelo scanf
+
         fprintf(fp, "%s %d\n", a.nome, a.idade);
     }
 
     fclose(fp);
+    printf("Dados salvos com sucesso em 'alunos.txt'.\n");
+
     return 0;
 }
